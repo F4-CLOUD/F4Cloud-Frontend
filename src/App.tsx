dc67f2466b7b221801de9ca00165193d19f84218
장규범 <jkb2221@gmail.com>
2021-04-02 01:02:55 +0900

    Refactor: remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
+import Router from './Router';
+import GlobalStyle from './styles/GlobalStyle';
+import { theme } from './styles/theme'
 
-function App() {
+const Wrapper = styled.div``;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle/>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+      </ThemeProvider>
+    </Wrapper>
   );
-}
+};
 
 export default App;
