ca7464231dfc0851e154110b83b8313a89f1c761
장규범 <jkb2221@gmail.com>
2021-04-03 03:13:31 +0900

    fix: 라우팅 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from './pages/HomePage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import UserCloudPage from './pages/UserCloudPage';
 
 const Router: React.FC = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/register" component={RegisterPage} />
+      <Route exact path={['/@:username/', '/']} component={UserCloudPage} />
+      <Route path="/@:username/:cloudId" component={UserCloudPage} />
       <Route path="/">404 not found</Route>
     </Switch>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,6 +11,9 @@ const HomePage: React.FC = () => {
   return (
     <Wrapper>
       <Text>F4CLOUD</Text>
+      <Text> 클라우드 소개 </Text>
+      <Text> 독보적인 검색 기능을 제공하는 F4CLOUD를 사용해보세요!</Text>
+      <Text> 주요 기능 </Text>
       <Link to="/login"> 로그인</Link>
     </Wrapper>
   );
