ef155794f3d56b972f3b70389d95d945de3aa5a9
장규범 <jkb2221@gmail.com>
2021-04-03 03:12:14 +0900

    feat: add register page

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import Text from '../components/Text';
+
+const Wrapper = styled.div``;
+
+const RegisterPage: React.FC = () => {
+  return (
+    <Wrapper>
+      <Text> 회원가입 </Text>
+    </Wrapper>
+  );
+};
+
+export default RegisterPage;
