0f74826e1e0b4b2b50ef5dde9b718fc8526419d4
장규범 <jkb2221@gmail.com>
2021-04-04 20:41:28 +0900

    fix: Homepage 라우팅 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,6 +15,8 @@ const HomePage: React.FC = () => {
       <Text> 독보적인 검색 기능을 제공하는 F4CLOUD를 사용해보세요!</Text>
       <Text> 주요 기능 </Text>
       <Link to="/login"> 로그인</Link>
+      <Link to="/register"> 회원가입</Link>
+      <Link to="/@:username/"> 유저 클라우드 </Link>
     </Wrapper>
   );
 };
