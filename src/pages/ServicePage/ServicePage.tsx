3080d24f50087b4a5862727ee8a5fe317495754f
Kyubum Jang <jkb2221@gmail.com>
2021-05-18 19:41:12 +0900

    fix: ServicePage 수정

---
+++ src/pages/ServicePage/ServicePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import Text from '../../components/Text';
+
+const Wrapper = styled.div``;
+
+const ServicePage: React.FC = () => {
+  return (
+    <Wrapper>
+      <Text>Service</Text>
+    </Wrapper>
+  );
+};
+
+export default ServicePage;
