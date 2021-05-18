6b041ed7dc32c69d7019b15b4f32f443f6be812f
Kyubum Jang <jkb2221@gmail.com>
2021-05-18 19:40:44 +0900

    fix: UserCloudPage 수정

--- src/pages/UserCloudPage/UserCloudPage.tsx
+++ src/pages/UserCloudPage/UserCloudPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Text from '../components/Text';
+import Text from '../../components/Text';
 
 const Wrapper = styled.div``;
 
