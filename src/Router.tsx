97d7a174e07ca607ba7cf583efb71be573051f73
Kyubum Jang <jkb2221@gmail.com>
2021-05-18 19:40:29 +0900

    fix: 라우팅 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import UserCloudPage from './pages/UserCloudPage';
+import HomePage from './pages/Homepage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import ContactPage from './pages/ContactPage/ContactPage';
+import ServicePage from './pages/ServicePage/ServicePage';
+import UserCloudPage from './pages/UserCloudPage/UserCloudPage';
 
 const Router: React.FC = () => {
   return (
@@ -11,6 +14,9 @@ const Router: React.FC = () => {
       <Route exact path="/" component={HomePage} />
       <Route exact path="/login" component={LoginPage} />
       <Route exact path="/register" component={RegisterPage} />
+      <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/service" component={ServicePage} />
+      <Route exact path="/contact" component={ContactPage} />
       <Route exact path={['/@:username/', '/']} component={UserCloudPage} />
       <Route path="/@:username/:cloudId" component={UserCloudPage} />
       <Route path="/">404 not found</Route>
