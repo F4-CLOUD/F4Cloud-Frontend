5c1e4408a3a050db02b0519e55d4886094b1e390
KyubumJang <jkb2221@gmail.com>
2021-06-04 13:21:29 +0900

    fix: index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@ ReactDOM.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
